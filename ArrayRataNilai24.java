bf40a5dcd111a7fdfe2e8f335fbef1466e2e42d6
Wahdaapf <wahdaadella.ba@gmail.com>
2024-10-30 02:48:32 +0100

    feat : make it more better

--- ArrayRataNilai24.java
+++ ArrayRataNilai24.java
@@ -4,9 +4,14 @@ public class ArrayRataNilai24 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int [] nilaiMhs = new int[10];
+        int jumlahMhs = 0;
+        System.out.print("Masukkan Jumlah Mahasiswa : ");
+        jumlahMhs = input.nextInt();
+
+        int [] nilaiMhs = new int[jumlahMhs];
         double total = 0;
         double rata2;
+        int totalNilaiDiatas70 = 0;
 
         for(int i = 0; i < nilaiMhs.length; i++){
             System.out.print("Masukkan Nilai Akhir Mahasiswa Ke-" + (i+1) + " : ");
@@ -15,9 +20,15 @@ public class ArrayRataNilai24 {
 
         for(int i = 0; i < nilaiMhs.length; i++){
             total += nilaiMhs[i];
+            if(nilaiMhs[i] > 70) {
+                totalNilaiDiatas70++;
+            }
         }
 
         rata2 = total / nilaiMhs.length;
         System.out.println("Nilai Rata Rata : " + rata2);
+        System.out.println("Jumlah Mahasiswa Yang mendapatkan nilai diatas 70 : " + totalNilaiDiatas70);
+
+        input.close();
     }
 }
